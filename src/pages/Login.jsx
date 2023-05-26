19591def05a9d194da7c483a1f656a0573353cf4
Sohel Rana <expartdesigns5@gmail.com>
2023-05-27 00:14:30 +0600

    login pages done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React from "react";
+// material
+import { Grid, TextField, Button } from "@mui/material";
+// images input
+import images from "../assets/login.png";
+import google from "../assets/google.png";
+// components
+import RegisterHeadding from "../components/RegisterHeadding";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+    return (
+        <Grid container spacing={2}>
+            <Grid item xs={6}>
+                <div className="regleft">
+                    <RegisterHeadding
+                        className="regheadding"
+                        title="Login to your account!"
+                    />
+                    <div>
+                        <img className="logingoogle" src={google} />
+                    </div>
+                    <div className="loginInput">
+                        <TextField
+                            id="standard-basic"
+                            label="Email Addres"
+                            variant="standard"
+                        />
+                    </div>
+                    <div className="loginInput">
+                        <TextField
+                            id="standard-basic"
+                            label="Password"
+                            variant="standard"
+                        />
+                    </div>
 
-export default Login
+                    <div className="regbutton">
+                        <Button variant="text">Login to Continue</Button>
+                    </div>
+                    <div>
+                        <span className="rgbspan">
+                            Don’t have an account ?{" "}
+                            <Link to="/" className="reglink">
+                                Sign up
+                            </Link>
+                        </span>
+                    </div>
+                </div>
+            </Grid>
+            <Grid item xs={6}>
+                <img className="regimg" src={images} />
+            </Grid>
+        </Grid>
+    );
+};
+
+export default Login;
