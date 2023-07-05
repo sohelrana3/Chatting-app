0bec34704c17dde6b288fc8a89545a597f28ae70
Sohel Rana <expartdesigns5@gmail.com>
2023-07-05 21:17:55 +0600

    fonget

--- src/pages/Forgetpassword.jsx
+++ src/pages/Forgetpassword.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { TextField, Button } from "@mui/material";
 
 const Forgetpassword = () => {
-  return (
-    <div>Forgetpassword</div>
-  )
-}
+    return (
+        <div className="forget">
+            <div className="forgetbody">
+                <h2>Forgot Your Password </h2>
+                <h6>Please Enter Your Email address</h6>
+                <TextField
+                    id="outlined-basic"
+                    label="Forgot Password"
+                    variant="outlined"
+                />
+                <div className="forbutton">
+                    <Button variant="contained">Contained</Button>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-export default Forgetpassword
+export default Forgetpassword;
