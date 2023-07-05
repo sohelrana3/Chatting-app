656082b840d50b860b0f4ebec46015d67437b9cc
Sohel Rana <expartdesigns5@gmail.com>
2023-07-05 21:49:46 +0600

    forgot password

--- src/pages/Forgetpassword.jsx
+++ src/pages/Forgetpassword.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextField, Button } from "@mui/material";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 const Forgetpassword = () => {
+    const auth = getAuth();
+    let navigate = useNavigate();
+    let [text, settext] = useState("");
+    let handleforgot = () => {
+        sendPasswordResetEmail(auth, text)
+            .then(() => {
+                // Password reset email sent!
+                // ..
+                navigate("/login");
+                console.log("forget");
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // ..
+            });
+    };
     return (
         <div className="forget">
             <div className="forgetbody">
@@ -11,9 +30,12 @@ const Forgetpassword = () => {
                     id="outlined-basic"
                     label="Forgot Password"
                     variant="outlined"
+                    onChange={(e) => settext(e.target.value)}
                 />
                 <div className="forbutton">
-                    <Button variant="contained">Contained</Button>
+                    <Button onClick={handleforgot} variant="contained">
+                        Forgrt Your Password
+                    </Button>
                 </div>
             </div>
         </div>
